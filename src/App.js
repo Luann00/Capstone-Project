3a29619e361c2afb5ee481e26fbdb06e4ce66cc7
rileynguye <rivenareid@gmail.com>
2023-11-08 10:30:16 +0100

    fenster aktualisiert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+import './App.css';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MDBContainer fluid className="p-3 my-5 h-custom">
+
+      <MDBRow>
+
+        {/* <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" />
+        </MDBCol> */}
+
+        <MDBCol col='4' md='6'>
+
+
+          <div className="divider d-flex align-items-center my-4">
+            <p className="text-center fw-bold mx-3 mb-0">Anmeldung</p>
+          </div>
+
+          <MDBInput wrapperClass='mb-4' label='Benutzername' id='formControlLg' type='email' size="lg"/>
+          <MDBInput wrapperClass='mb-4' label='Passwort' id='formControlLg' type='password' size="lg"/>
+
+          <div className="d-flex justify-content-between mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Erinnern mich' />
+            <a href="!#">Passwort vergessen?</a>
+          </div>
+
+          <div className='text-center text-md-start mt-4 pt-2'>
+            <MDBBtn className="mb-0 px-5" size='lg'>Melde an</MDBBtn>
+            
+          </div>
+
+        </MDBCol>
+
+      </MDBRow>
+
+
+        
+
+         
+
+      
+
+    </MDBContainer>
   );
 }
 
 export default App;
+
