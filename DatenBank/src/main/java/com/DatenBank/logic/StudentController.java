92e63a7c26862520942650f0516091ffb4224dcb
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-11-10 02:05:48 +0100

    added crossorigin tag to controller

--- DatenBank/src/main/java/com/DatenBank/logic/StudentController.java
+++ DatenBank/src/main/java/com/DatenBank/logic/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller // This means that this class is a Controller
+@CrossOrigin
 @RequestMapping("/student") // This means URL's start with /demo (after Application path)
 public class StudentController {
 	
