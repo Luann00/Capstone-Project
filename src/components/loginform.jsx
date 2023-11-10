6269ee26628b2e880969d54b480b31cf24a4fd34
rileynguye <rivenareid@gmail.com>
2023-11-10 17:26:03 +0100

    Whitelist Studenten aktualisieren

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-// import GoogleLogin from "react-google-login";
-// import { gapi } from "gapi-script";
 import "./loginform.css";
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
 
 
 const LoginForm = () => {
@@ -11,14 +8,12 @@ const LoginForm = () => {
 
     return (
         <div className="cover">
-            <h1>Anmeldung</h1>
+            <h1 className="title">Anmeldung</h1>
             <input type="text" placeholder="Benutzername" />
             <input type="password" placeholder="Passwort" />
             <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Erinnern mich' />
-            <a href="!#">Passwort vergessen?</a>
+            <a className= "pass-vergessen" href="!#">Passwort vergessen?</a>
           </div>
-
             <div className="login-btn">Melde an</div>
 
             
