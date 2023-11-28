902ebbd722156bdb7f864a66949620e12754af53
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-11-28 15:04:05 +0100

    maked navbar respsonsive for smalelr devices

--- src/components/NavbarAdmin.jsx
+++ src/components/NavbarAdmin.jsx
@@ -9,26 +9,31 @@ function NavbarAdmin() {
   return (
     <div className="Navbar">
       <>
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <LinkContainer to="/Home">
-                <Nav.Link>Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/StudentTable">
-                <Nav.Link>Students</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/UniversityTable">
-                <Nav.Link>Universities</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/WhitelistStudent">
-                <Nav.Link>WhitelistStudent</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/WhitelistVerwalter">
-                <Nav.Link>WhitelistVerwalter</Nav.Link>
-              </LinkContainer>
-            </Nav>
+            <LinkContainer to="/Home">
+              <Navbar.Brand>Navbar</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <LinkContainer to="/Home">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/StudentTable">
+                  <Nav.Link>Students</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/UniversityTable">
+                  <Nav.Link>Universities</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/WhitelistStudent">
+                  <Nav.Link>WhitelistStudent</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/WhitelistVerwalter">
+                  <Nav.Link>WhitelistVerwalter</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </>
