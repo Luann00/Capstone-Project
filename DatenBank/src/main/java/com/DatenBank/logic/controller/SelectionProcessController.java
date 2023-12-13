c34eef935a96751cf3d495bba510330eec07635d
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-12-13 17:25:29 +0100

    added backend configuration for editing selecitonprocess

--- DatenBank/src/main/java/com/DatenBank/logic/controller/SelectionProcessController.java
+++ DatenBank/src/main/java/com/DatenBank/logic/controller/SelectionProcessController.java
@@ -12,46 +12,60 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.DatenBank.logic.entity.SelectionProcess;
+import com.DatenBank.logic.entity.Student;
 import com.DatenBank.logic.service.SelectionProcessService;
 
 @Controller // This means that this class is a Controller
 @CrossOrigin
 @RequestMapping("/selectionProcess")
 public class SelectionProcessController {
-	
 
 	@Autowired
 	private SelectionProcessService selectionProcessService;
-	
-	public SelectionProcessController(SelectionProcessService selectionProcessService) {
-        this.selectionProcessService = selectionProcessService;
-    }
-	
-	@PostMapping
-    public ResponseEntity<SelectionProcess> addSelectionProcess(@RequestBody SelectionProcess selectionProcess) {
-		selectionProcessService.addSelectionProcess(selectionProcess);
-        return new ResponseEntity<>(selectionProcess, HttpStatus.CREATED);
-    }
-	
-	
-	@DeleteMapping("/{year}")
-    public ResponseEntity<?> deleteSelectionProcess(@PathVariable int year) {
-		selectionProcessService.deleteSelectionProcess(year);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 
-	
-	
+	public SelectionProcessController(SelectionProcessService selectionProcessService) {
+		this.selectionProcessService = selectionProcessService;
+	}
+
+	@PostMapping
+	public ResponseEntity<SelectionProcess> addSelectionProcess(@RequestBody SelectionProcess selectionProcess) {
+		selectionProcessService.addSelectionProcess(selectionProcess);
+		return new ResponseEntity<>(selectionProcess, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{year}")
+	public ResponseEntity<?> deleteSelectionProcess(@PathVariable int year) {
+		selectionProcessService.deleteSelectionProcess(year);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PutMapping("/{year}")
+	public ResponseEntity<Student> updateStudent(
+			@PathVariable int year,
+			@RequestBody SelectionProcess updatedProcess) {
+
+		try {
+			Student result = selectionProcessService.updateProcess(year, updatedProcess);
+			return ResponseEntity.ok(result);
+		} catch (ClassNotFoundException e) {
+			// Handle not found exception
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			// Handle other exceptions
+			return ResponseEntity.status(500).build();
+		}
+	}
+
 	@GetMapping
 	public ResponseEntity<List<SelectionProcess>> getAllSelectionProcess() {
-	    List<SelectionProcess> selectionProcess = new ArrayList<SelectionProcess>();
-	    selectionProcess = selectionProcessService.getAllSelectionProcess();
-	    return new ResponseEntity<>(selectionProcess, HttpStatus.OK);
+		List<SelectionProcess> selectionProcess = new ArrayList<SelectionProcess>();
+		selectionProcess = selectionProcessService.getAllSelectionProcess();
+		return new ResponseEntity<>(selectionProcess, HttpStatus.OK);
 	}
-	
 
 }
