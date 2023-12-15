32c61b3e3f8eb75667a1497c9111166a642cebb1
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-12-15 23:27:47 +0100

    removed unicardpage from navbar admin

--- src/components/NavigationBar/NavbarAdmin.jsx
+++ src/components/NavigationBar/NavbarAdmin.jsx
@@ -25,10 +25,6 @@ function NavbarAdmin() {
                 <Link to="/WhitelistStudent" className="nav-link">WhitelistStudent</Link>
                 <Link to="/WhitelistVerwalter" className="nav-link">WhitelistVerwalter</Link>
                 <Link to="/SelectionProcess" className="nav-link">SelectionProcess</Link>
-                
-                <Link to="/UniCardPage" className="nav-link">UniCardPage</Link>
-                
-
               </Nav>
             </Navbar.Collapse>
           </Container>
