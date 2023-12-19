536658b35f6318f01a7152cd98d4c34ad1596287
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-12-19 16:27:05 +0100

    fetched studentID and its respective 3 preferences to show it in the table

--- src/components/HomePage/HomePageAdmin.jsx
+++ src/components/HomePage/HomePageAdmin.jsx
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 const HomePageAdmin = () => {
     const [universities, setUniversities] = useState([]);
     const [students, setStudents] = useState([]);
+    const [studentPreferences, setStudentPreferences] = useState({});
+
 
 
     useEffect(() => {
@@ -17,23 +19,69 @@ const HomePageAdmin = () => {
             }
         };
 
-        fetchUniversities();
-    }, []);
-
-
-    useEffect(() => {
         const fetchStudents = async () => {
-          try {
-            const response = await fetch('http://localhost:8081/student');
-            const data = await response.json();
-            setStudents(data);
-          } catch (error) {
-            console.log('Error fetching data:', error);
-          }
+            try {
+                const response = await fetch('http://localhost:8081/student');
+                const data = await response.json();
+                setStudents(data);
+            } catch (error) {
+                console.log('Error fetching data:', error);
+            }
         };
-    
+
+        const fetchStudentPreferences = async () => {
+            try {
+                const preferences = await Promise.all(
+                    students.map(async (student) => {
+                        const response = await fetch(`http://localhost:8081/student/${student.matrikelnummer}/priorities`);
+                        const prefs = await response.json();
+                        return { studentID: student.matrikelnummer, preferences: prefs };
+                    })
+                );
+
+                
+                const preferencesMap = preferences.reduce((acc, { studentId, preferences }) => {
+                    return Object.assign(acc, { [studentId]: preferences });
+                    // Alternatively, you can use the spread operator:
+                    // return { ...acc, [studentId]: preferences };
+                }, {});
+                
+
+            } catch (error) {
+                console.log('Error fetching student preferences:', error);
+            }
+        };
+
+        fetchUniversities();
         fetchStudents();
-      }, []);
+        fetchStudentPreferences();
+    }, [students]);
+
+
+
+    const studentNames = students.map((student) => `${student.vorname} ${student.nachname}`);
+    const studentMatrikelnumbers = students.map((student) => student.matrikelnummer);
+
+    const fillTableCells = (uniID) => {
+        return students.map((student, index) => {
+            const preferences = studentPreferences[student.id] || [];
+            let matchingPrefOrder = '';
+
+            // Iterate through each preference for the current student
+            for (let i = 0; i < preferences.length; i++) {
+                const currentPref = preferences[i];
+
+                // Check if the current preference matches the uniID
+                if (currentPref.universityId === uniID) {
+                    matchingPrefOrder = currentPref.preferenceOrder;
+                    break; // Exit the loop if a match is found
+                }
+            }
+
+            return <td key={index}>{matchingPrefOrder}</td>;
+        });
+    };
+
 
     return (
         <div>
@@ -44,7 +92,9 @@ const HomePageAdmin = () => {
                         <th>Uni Name</th>
                         <th>Slots</th>
                         <th>FirstPrio</th>
-
+                        {studentMatrikelnumbers.map((studentName, index) => (
+                            <th key={index}>{studentName}</th>
+                        ))}
                         {/* Add more headers as needed */}
                     </tr>
                 </thead>
@@ -54,8 +104,8 @@ const HomePageAdmin = () => {
                             <td>{university.name}</td>
                             <td>{university.slots}</td>
                             <td>{university.firstPref}</td>
+                            {fillTableCells(university.id)}
 
-                            {/* Add more columns as needed */}
                         </tr>
                     ))}
                 </tbody>
