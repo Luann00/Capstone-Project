3155b18b07a8fc351911906cac63a2fb0c9c6368
Luan Zekiri <lzekiri@smail.uni-koeln.de>
2023-12-19 15:40:54 +0100

    begin to implement end table for admin

--- src/components/HomePage/HomePageAdmin.jsx
+++ src/components/HomePage/HomePageAdmin.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const service = () => {
+//Definiere in dieser Klasse die Endtabelle für den Admin mit allen wichtigen Informationen
+const HomePageAdmin = () => {
+    const [universities, setUniversities] = useState([]);
+    const [students, setStudents] = useState([]);
+
+
+    useEffect(() => {
+        const fetchUniversities = async () => {
+            try {
+                const response = await fetch('http://localhost:8081/university');
+                const data = await response.json();
+                setUniversities(data);
+            } catch (error) {
+                console.log('Error fetching data:', error);
+            }
+        };
+
+        fetchUniversities();
+    }, []);
+
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+          try {
+            const response = await fetch('http://localhost:8081/student');
+            const data = await response.json();
+            setStudents(data);
+          } catch (error) {
+            console.log('Error fetching data:', error);
+          }
+        };
+    
+        fetchStudents();
+      }, []);
+
     return (
         <div>
             <h1>This is the main page for the admin!</h1>
-        </div>
-    )
-}
+            <table className="table table-striped table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>Uni Name</th>
+                        <th>Slots</th>
+                        <th>FirstPrio</th>
 
-export default service
+                        {/* Add more headers as needed */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {universities.map((university) => (
+                        <tr key={university.id}>
+                            <td>{university.name}</td>
+                            <td>{university.slots}</td>
+                            <td>{university.firstPref}</td>
+
+                            {/* Add more columns as needed */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default HomePageAdmin;
