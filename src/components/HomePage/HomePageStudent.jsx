2ab450e0c6936b8b54d54592b272bdf640f80d00
rileynguye <rivenareid@gmail.com>
2024-01-13 19:44:01 +0100

    Add homepage styling and import CSS file

--- src/components/HomePage/HomePageStudent.jsx
+++ src/components/HomePage/HomePageStudent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PrivacyPage from "../../components/InformationPrivacy/InformationPrivacyPage"
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import './HomePageStudent.css';
 
 
 const HomeStudent = ({ onAccept }) => {
@@ -28,15 +29,15 @@ const HomeStudent = ({ onAccept }) => {
 
 
   return (
-    <div>
+    <div className="homepage">
       {acceptedPolicy ? (
         <>
-        <h1>Hello, {name}!</h1>
+        <h2>Welcome, {name}!</h2>
       <h3>Here's how to get started:</h3>
         <Container className="mt-5">
         <Row>
           <Col>
-            <InstructionCard
+            <InstructionCard className="instruction-card"
               title="1.Explore & Choose"
               content="Head to the 'Universities' section.The universities's information would be displayed in the card. Click on the university card title to view more information about the university.You can also filter the universities by country, city, as well as search for a specific university name."
             />
@@ -44,7 +45,7 @@ const HomeStudent = ({ onAccept }) => {
         </Row>
         <Row>
           <Col>
-            <InstructionCard
+            <InstructionCard className="instruction-card"
             title="2.Pick your preferred universities"
             content="Tap on the 'Choose preferences' dropdown on your preferred university card. Select the university as your 1st Priority, 2nd Priority or 3rd Priority.Each priority can only be selected once."
               
@@ -53,7 +54,7 @@ const HomeStudent = ({ onAccept }) => {
         </Row>
         <Row>
           <Col>
-            <InstructionCard
+            <InstructionCard className="instruction-card"
               title="3.Review your preferences"
               content="Go to 'Your preferences'. Here you can reset your priorities one by one or all of them together."
             />
@@ -61,16 +62,16 @@ const HomeStudent = ({ onAccept }) => {
         </Row>
         <Row>
           <Col>
-            <InstructionCard
-              title="Time management"
+            <InstructionCard className="instruction-card"
+              title="4.Time management"
               content="Make sure that you finalize your decision within the time frame."
             />
           </Col>
         </Row>
         <Row>
           <Col>
-            <InstructionCard
-              title="Stay Updated"
+            <InstructionCard className="instruction-card"
+              title="5.Stay Updated"
               content="We'll send you an email as soon as the decision is made."
             />
           </Col>
